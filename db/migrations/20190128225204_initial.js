059cd088d6be1c595a122f8632bc8973c0e3464a
Corina Allen <StarPerfect@gmail.com>
2019-12-03 10:57:19 -0700

    Remove excess migrations

--- db/migrations/20190128225204_initial.js
+++ db/migrations/20190128225204_initial.js
@@ -5,30 +5,29 @@ exports.up = function(knex) {
 		table.string('email');
 		table.string('password_hash');
 		table.string('api_key');
-  
+
 		table.timestamps(true, true);
 	  }),
-  
+
 	  knex.schema.createTable('favorites', function(table) {
 		table.increments('id').primary();
 		table.string('title');
 		table.string('artistName');
-		table.string('genre');
+		table.string('genre').defaultTo("Unknown");
 		table.integer('rating');
 		table.integer('user_id').unsigned()
 		table.foreign('user_id')
 		  .references('users.id');
-  
+
 		table.timestamps(true, true);
 	  })
 	])
   };
-  
-  
+
+
   exports.down = function(knex) {
 	return Promise.all([
 	  knex.schema.dropTable('favorites'),
 	  knex.schema.dropTable('users')
 	]);
   }
-
