ca1bceac955723c0757fbe5fe36788df8341e7c4
Corina Allen <StarPerfect@gmail.com>
2019-12-05 16:12:12 -0700

    Create PlaylistsController with CRU functionality
    Co-authored-by: evettetelyas <etelyas@gmail.com>

---
+++ app/controllers/playlistsController.js
@@ -0,0 +1,50 @@
+const Playlist = require('../models/playlist')
+
+const create = (request, response) => {
+	Playlist.findByTitle(request.body.title)
+		.then((titles) => {
+			if (titles[0]) {
+				response.status(400).json({message: `Playlist with ${request.body.title} already exists, please rename.`})
+			}
+			else {
+				    Playlist.create(request.body)
+						.then((id) => {
+							Playlist.find(parseInt(id))
+								.then((list) => response.status(201).json(list))
+			})
+						.catch(error => response.status(400).json(error))		
+				}
+		})
+}
+
+const index = (request, response) => {
+	Playlist.all()
+	.then((lists) => response.status(200).json(lists))
+	.catch(error => response.status(400).json(error))
+}
+
+const update = (request, response) => {
+	Playlist.find(request.params.id)
+		.then((list) => {
+		if (list[0]) {
+			Playlist.update(list[0].id, request.body)
+				.then((revisedList) => {
+					Playlist.find(revisedList)
+						.then((newObj) => {
+							response.status(200).json(newObj)
+				})
+				})
+		} else {
+			response.status(404).json({
+				message: `Playlist not found with id ${request.params.id}`
+			})
+		}
+	})
+	.catch(() => response.status(500).json())
+}
+
+
+module.exports = 	{	create, 
+						index,
+						update
+					};
