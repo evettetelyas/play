204992794f177580df3e91972f0cb4440f2f26a1
evettetelyas <etelyas@gmail.com>
2019-12-09 15:18:29 -0700

    fix testing suite but still have commented tests

--- tests/playlistsController.spec.js
+++ tests/playlistsController.spec.js
@@ -1,9 +1,9 @@
-// var shell = require('shelljs');
-// var request = require("supertest");
-// var app = require('../app');
-// const Playlist = require('../app/models/playlist')
+var shell = require('shelljs');
+var request = require("supertest");
+var app = require('../app');
+const Playlist = require('../app/models/playlist')
 
-// describe('Test Playlist Controller functionality', () => {
+describe('Test Playlist Controller functionality', () => {
 //     describe('test creating a list', () => {
 //         it('happy path', async () => {
 //             let body = { "title": "My New Thingz" }
@@ -15,30 +15,30 @@
 //             expect(res.body[0].title).toBe("My New Thingz")
 //         });
 
-//         it('sad path', async () => {
-//             let list = 0;
+        it('sad path', async () => {
+            let list = 0;
 
-//             let body = { "title": "siqq jamz"}
-//             const res = await request(app)
-//             .post('/api/v1/playlists')
-//             .send(body)
+            let body = { "title": "siqq jamz"}
+            const res = await request(app)
+            .post('/api/v1/playlists')
+            .send(body)
 
-//             expect(res.statusCode).toBe(400);
-//             expect(res.body.message).toBe(`Playlist with siqq jamz already exists, please rename.`)
-//         });
-//     }); 
+            expect(res.statusCode).toBe(400);
+            expect(res.body.message).toBe(`Playlist with siqq jamz already exists, please rename.`)
+        });
+    }); 
 
-//     describe('test getting all playlist', () => {
-//         it('happy path', async () => {
-//             const res = await request(app)
-//                 .get('/api/v1/playlists')
+    describe('test getting all playlist', () => {
+        it('happy path', async () => {
+            const res = await request(app)
+                .get('/api/v1/playlists')
 
-//             expect(res.statusCode).toBe(200);
-//             expect(res.body[0]).toHaveProperty('title')
-//         });
-//     });
+            expect(res.statusCode).toBe(200);
+            expect(res.body[0]).toHaveProperty('title')
+        });
+    });
 
-//     describe('test updating playlist', () => {
+    describe('test updating playlist', () => {
 //         let list = 0;
 
 //         Playlist.findByTitle("My New Thingz")
@@ -55,18 +55,18 @@
 //             expect(res.body[0].title).toBe("Ruby is Better")
 //         })
 
-//         it('test sad update path', async () => {
-//             body = { "title": "Ruby is Better"}
+        it('test sad update path', async () => {
+            body = { "title": "Ruby is Better"}
 
-//             const res = await request(app)
-//                 .put('/api/v1/playlists/1999')
-//                 .send(body)
+            const res = await request(app)
+                .put('/api/v1/playlists/1999')
+                .send(body)
 
-//             expect(res.statusCode).toBe(404);
-//         })
-//     })
+            expect(res.statusCode).toBe(404);
+        })
+    })
 
-//     describe('test deleting list', () => {
+    describe('test deleting list', () => {
 //         let list = 0;
 
 //         const deleteFunction = Playlist.findByTitle("Ruby is Better")
@@ -82,11 +82,10 @@
 //             expect(res.statusCode).toBe(204);
 //         });
 
-//         it('sad path', async () => {
-//             const res = await request(app)
-//             .delete('/api/v1/playlists/1999')
+        it('sad path', async () => {
+            const res = await request(app)
+            .delete('/api/v1/playlists/1999')
 
-//             expect(res.statusCode).toBe(404);
-//         }); 
-//     });
-// });
+            expect(res.statusCode).toBe(404);
+        }); 
+    });
